e967fb00953e07e3eba83004d1bcbd5289eb2f19
Arka-ops07 <dasarka2007@gmail.com>
2026-04-22 20:46:15 +0530

    final working local version with tailwind fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-<<<<<<< HEAD
-    "./src/**/*.{js,ts,jsx,tsx}",  // 🔥 THIS LINE FIXES EVERYTHING
-=======
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
->>>>>>> decc78a3f2a7a6bb520f0b990d2eb561da45945c
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> decc78a3f2a7a6bb520f0b990d2eb561da45945c
